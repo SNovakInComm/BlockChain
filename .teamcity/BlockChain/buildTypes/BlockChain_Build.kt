79393bc7f48024f5244d5d75b6344c83e431ab98
snovak <snovak@TeamCity>
2017-12-15 11:28:43 -0800

    TeamCity change in 'BlockChain' project: runners of 'Build' build configuration were updated

--- .teamcity/BlockChain/buildTypes/BlockChain_Build.kt
+++ .teamcity/BlockChain/buildTypes/BlockChain_Build.kt
@@ -5,6 +5,7 @@ import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.VisualStudioStep
 import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.ant
 import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.dotnetBuild
 import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.dotnetRestore
+import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.visualStudio
 import jetbrains.buildServer.configs.kotlin.v2017_2.triggers.vcs
 
@@ -44,6 +45,10 @@ object BlockChain_Build : BuildType({
             name = "Step1"
             args = "echo Step1"
             param("dotNetCoverage.dotCover.home.path", "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%")
+        }
+        script {
+            name = "1 (1)"
+            scriptContent = "Echo 1"
         }
     }
 
